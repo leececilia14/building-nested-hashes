cf3d8891db0c4e4dddc2c3ba9fbb536def58e317
Cecilia Lee <leececilia14@gmail.com>
2020-11-23 02:24:13 +0900

    Done.

--- lib/fifth_challenge.rb
+++ lib/fifth_challenge.rb
@@ -23,9 +23,7 @@ def fifth_challenge
    }
   }
 
-  #code your solution here:
-
-
-  #Don't touch the following line! The method must return our newly modified epic tragedy hash
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   epic_tragedy
 end
